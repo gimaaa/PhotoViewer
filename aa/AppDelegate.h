58214857122ae43985c3c2439aa701ed8d9da353
JinProMan <KB@sadfdeMac-mini.local>
2015-06-15 16:42:25 +0800

    1.0

---
+++ aa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aa
+//
+//  Created by KimBox on 15/6/12.
+//  Copyright (c) 2015年 KimBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
